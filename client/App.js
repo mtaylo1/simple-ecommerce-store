cf935d531781343a0917f1440109b0264099d48f
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2023-05-18 17:05:50 -0400

    adding some files for react install

---
+++ client/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render(){
+    return ( 
+        <div className="App">
+          <h1>Testing!</h1>
+        </div> 
+    );
+  }
+}
+
+export default App;
